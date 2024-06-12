b95e52dd32f6871a3b0b5f43cbd49f76fcf67005
Nawat Pimolsathien <nawatpim@gmail.com>
2024-06-12 13:58:21 +0700

    edit profile

--- src/app/(main-system)/board/page.tsx
+++ src/app/(main-system)/board/page.tsx
@@ -1,16 +1,11 @@
 'use client'
-
 import BoardList from "@/components/Board/BoardList"
-import { ThemeProvider } from "@/components/ControlSystem/themeSet"
-import Sidebar from "@/components/ControlSystem/sideBar"
 
 export default function BoardPage () {
 
-    return (
-        <ThemeProvider>
+    return (       
         <main className="h-full bg-orange-50 ml-64">
             <BoardList starred={false}></BoardList>
         </main>
-        </ThemeProvider>
     )
 }
