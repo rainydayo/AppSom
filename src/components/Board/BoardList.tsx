838d97f08df399df6daefb5944ee4f8308a23468
unknown <jasonchawsuan@gmail.com>
2024-06-13 21:50:04 +0700

    qol

--- src/components/Board/BoardList.tsx
+++ src/components/Board/BoardList.tsx
@@ -2,11 +2,12 @@
 
 import { useAppSelector } from "@/redux/store";
 import { useEffect, useState } from "react";
-import { Board, BoardJSON, User } from "../../../interface";
+import { Board, BoardJSON, List, User } from "../../../interface";
 import GetBoards from "@/lib/GetBoards";
 import CreateBoard from "@/lib/CreateBoard";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
+import CreateList from "@/lib/CreateList";
 
 export default function BoardList({ starred }: { starred: boolean }) {
     const [boards, setBoards] = useState<Board[]>([]);
@@ -20,7 +21,7 @@ export default function BoardList({ starred }: { starred: boolean }) {
     useEffect(() => {
         const LoadBoards = async () => {
             const data: BoardJSON = await GetBoards();
-            const filteredBoards = data.data.filter(b => b.favorite === starred);
+            const filteredBoards = data.data.filter(b => (b.favorite === starred && (b.owner === session.user.id || b.member.includes(session.user.id))));
             setBoards(filteredBoards);
         };
         LoadBoards();
@@ -40,9 +41,12 @@ export default function BoardList({ starred }: { starred: boolean }) {
 
 
     const uuid = crypto.randomUUID();
+    const uuid_l_1 = crypto.randomUUID();
+    const uuid_l_2 = crypto.randomUUID();
+    const uuid_l_3 = crypto.randomUUID();
 
     const board: Board = {
-        id: uuid,
+        id: crypto.randomUUID(),
         name: "My Board 3",
         description: "This is my board 3",
         lists: [],
@@ -51,6 +55,34 @@ export default function BoardList({ starred }: { starred: boolean }) {
         member: [session.user.id],
         color: ""
     };
+
+    const createTemplateList = async (bid: string) => {
+        const to_do: List = {
+            id: crypto.randomUUID(),
+            name: "To-Do",
+            description: "To-Do List",
+            cards: [],
+            board: bid
+        }
+        const doing: List = {
+            id: crypto.randomUUID(),
+            name: "Doing",
+            description: "Doing List",
+            cards: [],
+            board: bid
+        }
+        const done: List = {
+            id: crypto.randomUUID(),
+            name: "Done",
+            description: "Doing List",
+            cards: [],
+            board: bid
+        }
+        await CreateList(to_do, bid);
+        await CreateList(doing, bid);
+        await CreateList(done, bid);
+    }
+    
 
     return (
         <div className="flex flex-col">
@@ -81,7 +113,7 @@ export default function BoardList({ starred }: { starred: boolean }) {
             </div>
             <div className="flex flex-wrap flex-row justify-start gap-10 p-10">
                 <div className="bg-orange-400 rounded w-[300px] h-[150px]">
-                    <button className="w-full h-full p-5" onClick={() => { CreateBoard(board); }}>
+                    <button className="w-full h-full p-5" onClick={() => { CreateBoard(board); createTemplateList(board.id); }}>
                         <h1 className="text-white font-semibold">Create Board</h1>
                     </button>
                 </div>
--- src/components/ControlSystem/boardNav.tsx
+++ src/components/ControlSystem/boardNav.tsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react";
 import { Board } from "../../../interface";
 import Image from "next/image";
 import UpdateBoardById from "@/lib/UpdateBoardById";
+import { useSession } from "next-auth/react";
 
 export default function BoardNav ({board} : {board: Board}) {
     const [isStarActive, setIsStarActive] = useState<boolean>(board.favorite);
-
+    const { data: session } = useSession();
+    if (!session) {
+        return null;
+    }
     const handleStarClick = async () => {
         setIsStarActive(!board.favorite);
         const favorite: Board = {
@@ -24,7 +28,7 @@ export default function BoardNav ({board} : {board: Board}) {
     return (
         <div className="bg-orange-300 w-full h-8 flex flex-row justify-between items-center px-8 py-9">
             <div className="flex flex-row gap-3 items-center">
-                <h1 className="font-semibold text-3xl">{board.name}</h1>
+                <h1 className="font-semibold text-3xl">{board.name} {board.owner == session.user.id ? " (Owner)" : " (Member)"}</h1>
                 <Image 
                     src={isStarActive ? "/Image/star_Active.png" : "/Image/star_notActive.png"} 
                     alt="Star" 
